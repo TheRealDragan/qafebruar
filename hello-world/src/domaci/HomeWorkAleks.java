d8af44a251361bf1bb1480db97f62684f412a561
Aleksandra <aleks.milojevic@outlook.com>
2021-03-25 12:39:01 +0100

    Dodata klasa HomeWorkAleks

---
+++ hello-world/src/domaci/HomeWorkAleks.java
@@ -0,0 +1,37 @@
+package domaci;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+public class HomeWorkAleks {
+
+	public static void main(String[] args) {
+
+		Set<Integer> set1 = new HashSet<Integer>();
+		set1.add(2);
+		set1.add(3);
+		set1.add(4);
+		set1.add(5);
+
+		Set<Integer> set2 = new HashSet<Integer>();
+		set2.add(4);
+		set2.add(5);
+		set2.add(6);
+		set2.add(7);
+
+		// System.out.println(set1);
+		// System.out.println(set2);
+
+		// Set<Integer> setS = new HashSet<>(set1);
+		set1.retainAll(set2); // presek
+		// set1.addAll(Set2); //unija
+		// set1.removeAll(Set2); //razlika
+
+		for (int i : set1) {
+			System.out.println(i);
+		}
+
+	}
+
+}
