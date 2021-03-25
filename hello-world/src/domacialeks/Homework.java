401b5db72e023209128d283f11abfd9691fed6ce
Aleksandra <aleks.milojevic@outlook.com>
2021-03-25 22:49:11 +0100

    Dodata klasa Homework

--- hello-world/src/domacialeks/Homework.java
+++ hello-world/src/domacialeks/Homework.java
@@ -1,10 +1,9 @@
-package domaci;
+package domacialeks;
 
-import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 
-public class HomeWorkAleks {
+public class Homework {
 
 	public static void main(String[] args) {
 
@@ -13,20 +12,20 @@ public class HomeWorkAleks {
 		set1.add(3);
 		set1.add(4);
 		set1.add(5);
-
+        
 		Set<Integer> set2 = new HashSet<Integer>();
 		set2.add(4);
 		set2.add(5);
 		set2.add(6);
 		set2.add(7);
 
-		// System.out.println(set1);
-		// System.out.println(set2);
-
-		// Set<Integer> setS = new HashSet<>(set1);
-		set1.retainAll(set2); // presek
-		// set1.addAll(Set2); //unija
-		// set1.removeAll(Set2); //razlika
+		//System.out.println(set1);
+		//System.out.println(set2);
+		
+	//	Set<Integer> setS = new HashSet<>(set1);
+		set1.retainAll(set2);     //presek
+		//set1.addAll(Set2);      //unija
+		//set1.removeAll(Set2);   //razlika  
 
 		for (int i : set1) {
 			System.out.println(i);
@@ -35,3 +34,4 @@ public class HomeWorkAleks {
 	}
 
 }
+
